b5c959f3c39da91687fa347ca46fa78714058b7e
dserebro <dserebro@modelcode.ai>
2025-09-14 20:34:38 +0900

    init frontend env

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>MICCAI 2025 Papers Visualization</h1>
+        <p>Interactive graph visualization for exploring conference papers</p>
+      </header>
+      <main>
+        <div id="graph-container">
+          {/* Graph visualization will be implemented here */}
+          <p>Graph visualization coming soon...</p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
